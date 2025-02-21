89c48afc64e835303ba51fc17529abd94f426177
mfortelnyy <mfortelnyy@hawk.iit.edu>
2025-02-20 19:03:59 -0600

    Perform initial schema set up of DB

---
+++ Migrations/20250220062354_InitDB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace truckPRO_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogEntry");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogEntry",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ImageUrls = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsApprovedByManager = table.Column<bool>(type: "bit", nullable: false),
+                    LogEntryType = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogEntry", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LogEntry_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogEntry_UserId",
+                table: "LogEntry",
+                column: "UserId");
+        }
+    }
+}
